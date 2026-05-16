e1c43d9eaec7a4a23a893853ad9bcfc417b293e9
clementjnr <paulclement711@gmail.com>
2026-05-16 16:37:51 +0100

    update changes

---
+++ week02/Journal/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Journal journal = new Journal();
+
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("Select a choice from the menu: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine(prompt);
+
+                Console.Write("> ");
+
+                string response = Console.ReadLine();
+
+                Entry entry = new Entry();
+
+                entry._date = DateTime.Now.ToShortDateString();
+
+                entry._promptText = prompt;
+
+                entry._entryText = response;
+
+                journal.AddEntry(entry);
+            }
+
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+
+            else if (choice == 3)
+            {
+                Console.Write("What is the filename? ");
+
+                string file = Console.ReadLine();
+
+                journal.LoadFromFile(file);
+            }
+
+            else if (choice == 4)
+            {
+                Console.Write("What is the filename? ");
+
+                string file = Console.ReadLine();
+
+                journal.SaveToFile(file);
+            }
+        }
+    }
+}
